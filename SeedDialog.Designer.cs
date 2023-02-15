5e21f78087cb9cd961ab291ba47340298aa946b4
Andrew Sipes <AGSipes@student.fullsail.edu>
2023-02-15 10:14:57 -0500

    Created Seed DialogBox

---
+++ SeedDialog.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Game_of_Life
+{
+    partial class SeedDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.SeedLabel = new System.Windows.Forms.Label();
+            this.OKbutton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.RandomizeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(63, 32);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(123, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // SeedLabel
+            // 
+            this.SeedLabel.AutoSize = true;
+            this.SeedLabel.Location = new System.Drawing.Point(25, 34);
+            this.SeedLabel.Name = "SeedLabel";
+            this.SeedLabel.Size = new System.Drawing.Size(32, 13);
+            this.SeedLabel.TabIndex = 1;
+            this.SeedLabel.Text = "Seed";
+            // 
+            // OKbutton
+            // 
+            this.OKbutton.Location = new System.Drawing.Point(55, 84);
+            this.OKbutton.Name = "OKbutton";
+            this.OKbutton.Size = new System.Drawing.Size(83, 25);
+            this.OKbutton.TabIndex = 2;
+            this.OKbutton.Text = "OK";
+            this.OKbutton.UseVisualStyleBackColor = true;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelButton.Location = new System.Drawing.Point(155, 84);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(83, 25);
+            this.CancelButton.TabIndex = 3;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // RandomizeButton
+            // 
+            this.RandomizeButton.Location = new System.Drawing.Point(192, 30);
+            this.RandomizeButton.Name = "RandomizeButton";
+            this.RandomizeButton.Size = new System.Drawing.Size(71, 25);
+            this.RandomizeButton.TabIndex = 4;
+            this.RandomizeButton.Text = "Randomize";
+            this.RandomizeButton.UseVisualStyleBackColor = true;
+            // 
+            // SeedDialog
+            // 
+            this.AcceptButton = this.OKbutton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CancelButton;
+            this.ClientSize = new System.Drawing.Size(298, 111);
+            this.Controls.Add(this.RandomizeButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.OKbutton);
+            this.Controls.Add(this.SeedLabel);
+            this.Controls.Add(this.numericUpDown1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SeedDialog";
+            this.Text = "Seed";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label SeedLabel;
+        private System.Windows.Forms.Button OKbutton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button RandomizeButton;
+    }
+}
